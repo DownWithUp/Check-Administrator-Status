a0a40f0d7933e0873f7ee3c96c4b80b27d3254a9
StealthyByte <16905064+StealthyByte@users.noreply.github.com>
2018-01-08 11:03:22 -0500

    Create CheckAdminViaIsNTAdmin.asm

---
+++ CheckAdminViaIsNTAdmin.asm
@@ -0,0 +1,35 @@
+;No offical information on the "IsNtAdmin" API, meaning it might not be reliable, use at your own ristk.
+;https://source.winehq.org/WineAPI/IsNTAdmin.html
+Format PE Console
+include 'win32a.inc'
+entry start
+
+section '.data' data readable writable
+szIsAdmin db "You Have Admin Rights",0xA, 0
+szNotAdmin db "You Do Not Have Admin Rights",0xA, 0
+
+szAdv db 'advpack.dll',0
+
+section '.text' code readable writable executable
+start:
+        invoke IsNTAdmin, 0, 0
+        cmp eax, TRUE
+        jne IsNotAdmin
+IsAdmin:
+        invoke printf, szIsAdmin
+        invoke ExitProcess, 0
+IsNotAdmin:
+        invoke printf, szNotAdmin
+        invoke ExitProcess, 0
+
+
+section '.idata' import readable writable
+library kernel32, 'kernel32.dll',\
+        advpack, 'advpack.dll',\
+        msvcrt, 'msvcrt.dll'
+import advpack,\
+       IsNTAdmin, 'IsNTAdmin'
+import msvcrt,\
+       printf, 'printf'
+
+include 'api/kernel32.inc'
